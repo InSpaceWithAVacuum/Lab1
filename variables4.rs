3bf55a4a7211cd3dacd1aedc322f1c171afc7803
InSpaceWithAVacuum <113609980+InSpaceWithAVacuum@users.noreply.github.com>
2025-01-28 09:50:58 +0400

    commit 2
    Forgot to save variable 4 after modification.

--- variables4.rs
+++ variables4.rs
@@ -1,6 +1,7 @@
 // TODO: Fix the compiler error.
 fn main() {
-    let x = 3;
+    // We need to make the variable x modifiable. So we add mut
+    let mut x = 3;
     println!("Number {x}");
 
     x = 5; // Don't change this line
